eb8077fdcbc083a2fd1474c6e2d6cb799b341bec
Tyler Johnson <tjohnson@saltstack.com>
2019-08-01 17:30:38 -0600

    Removed ptable, doing it all locally without ssh

--- cloud_table.py
+++ cloud_table.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 import logging
 import subprocess
-import sysrsync
 
 from argparse import ArgumentParser
-from os import path
-from ptable import Table
+from os import environ, path, chdir
+from table import Table
 
 logger = logging.getLogger(__name__)
 
@@ -24,17 +23,15 @@ cloud_env = {
 }
 
 if __name__ == '__main__':
+    cloud_env.update(environ)
     logging.basicConfig(level=logging.DEBUG)
 
     parser = ArgumentParser(description='Wrapper around kitchen-salt')
 
     setup = parser.add_argument_group('Setup')
-    setup.add_argument('-o', '--cloud', type=str, help='SSH username@IP of the AWS instance/cloud creator', required=True, )
-    setup.add_argument('-R', '--local-root', type=str, help='full path to the local salt repo root',
+    setup.add_argument('-r', '--root', type=str, help='full path to the dev salt repo root',
                        default=path.expanduser('~/PycharmProjects/salt'))
-    setup.add_argument('-r', '--root', type=str, help=' full path to the salt repo root',
-                       default=path.expanduser('*/salt'))
-    setup.add_argument('platform', type=str, default='py3-centos-7', nargs='?',
+    setup.add_argument('-p', '--platform', type=str, default='py3-centos-7',
                        help='The specific platform to run on.  Run this script with --list to see available platforms')
 
     tests = parser.add_argument_group('Test Options')
@@ -50,27 +47,23 @@ if __name__ == '__main__':
     actions.add_argument('-v', '--verify', action='store_true', help='Verify the named machines and exit')
     actions.add_argument('-d', '--destroy', action='store_true', help='Destroy the named machines and exit')
     actions.add_argument('-L', '--login', action='store_true', help='Log into the named machine and exit', default='')
-    actions.add_argument('-p', '--preserve', action='store_true', help='Keep the named machines after the operations')
+    # This one is helpful if you are running --verify multiple times without any code changes
+    actions.add_argument('-R', '--preserve', action='store_true', help='Keep the named machines after the operations')
 
     args = parser.parse_args()
+    chdir(args.root)
     logger.debug("ARGS: {}".format(args))
-
-    # Grab the IP address from the ssh target
-    p = subprocess.Popen('ssh -G ' + args.cloud + "|awk '/^hostname / { print $2 }'", shell=True,
-                         stdout=subprocess.PIPE)
-
-    # Sync the local repository with the remote
-    sysrsync.run(source=args.local_root,
-                 destination=args.root,
-                 destination_ssh=args.cloud,
-                 options=['-a', '-v', '--delete'],
-                 exclusions=['.git', '.idea', '*.swp'],
-                 sync_source_contents=True,
-                 verbose=True, )
 
     # TODO Verify correct rbenv is installed
     # TODO install rbenv if needed
     # TODO verify bundle, kitchen, and GemFile are usable. Install missing things
+
+    # Run bundle update
+    logger.debug("Installing bundle from Gemfile")
+    if subprocess.Popen(['bundle', 'install'], env=cloud_env.copy()).wait():
+        logger.debug("Updating bundle from Gemfile")
+        subprocess.Popen(['bundle', 'update'], env=cloud_env.copy()).wait()
+
     # TODO verify docker is installed and service running
 
     # Configure Verification environment
@@ -80,47 +73,41 @@ if __name__ == '__main__':
         cloud_env['NOX_PASSTHROUGH_OPTS'] = '--run-expensive'
 
     # Set the shell on the cloud host to /bin/sh
-    table = Table(args.platform, nofail=args.no_fail, kitchen_cmd=['bundle', 'exec', 'kitchen'],
-                  shell='ssh {}'.format(args.cloud, args.root).format(args.cloud),
-                  init_cmd=[
-                      'bundle install --without ec2 windows macos opennebula vagrant --with docker',
-                      'bundle update',
-                  ],
-                  pre_cmd=[
-                      'cd {}'.format(args.root),
-                      ';'.join('export {}="{}"'.format(k, v) for k, v in cloud_env.items()),
-                  ])
-    instance = table.platform
+    table = Table([args.platform], nofail=args.no_fail, kitchen_cmd=['bundle', 'exec', 'kitchen'],
+                  env=cloud_env)
+    instance = table.active_machine
 
     # Add cloud options based on the instance name
     distro, version, pyver = instance.split('-')
-    cloud_env['CODECOV_FLAGS'] = '{distro}{version},{py},{transport}'.format(
+    table.env['CODECOV_FLAGS'] = '{distro}{version},{py},{transport}'.format(
         distro=distro, version=version, py=pyver, transport=transport
     )
-    cloud_env['TEST_SUITE'] = pyver
-    cloud_env['TEST_PLATFORM'] = distro
-    cloud_env['TEST_TRANSPORT'] = transport
+    table.env['TEST_SUITE'] = pyver
+    table.env['TEST_PLATFORM'] = distro
+    table.env['TEST_TRANSPORT'] = transport
 
     # If all the flags are false, then we will do all of them
     do_all = all(not flag for flag in [args.create, args.converge, args.verify, args.destroy, args.list, args.login])
     try:
         if do_all or args.create or args.login:
             table.create()
+            table.wait()
         if do_all or args.converge or args.login:
-            table.exec(
-                "ssh-agent /bin/bash -c 'ssh-add ~/.ssh/kitchen.pem; bundle exec kitchen converge {}'".format(instance),
-                shell=table.process[instance])
+            table.converge()
+        table.wait()
         if args.login:
-            table.login()
+            table.login(instance)
             args.preserve = True
-        if do_all or args.verify:
+            table.wait()
+        if do_all or args.test or args.verify:
             if args.test:
                 for t in args.test:
-                    table.verify(t)
+                    table.verify(test=t)
             else:
                 table.verify()
+            table.wait()
     finally:
-        print(table)
+        print(table.list)
         if args.list:
             exit(0)
         # Cleanup
@@ -128,5 +115,6 @@ if __name__ == '__main__':
             table.destroy()
         elif do_all and not args.preserve:
             table.destroy()
+        table.wait()
 
     logger.info("DONE!")
--- table.py
+++ table.py
@@ -5,35 +5,42 @@ import subprocess
 from argparse import ArgumentParser
 from os import chdir, environ, path
 from tqdm import trange
-from typing import List
+from typing import List, Dict
 
 logger = logging.getLogger(__name__)
 
 
 class Table:
     class KitchenList(set):
-        def __init__(self):
+        def __init__(self, kitchen_cmd: List[str] = None, env: Dict[str, str] = None, platform: str = None):
+            self.platform = platform if platform else ''
+            self.env = env
+            self.kitchen_cmd = kitchen_cmd if kitchen_cmd else ['kitchen']
             set.__init__(self, {x.decode() for x in
-                                subprocess.check_output(['kitchen', 'list', '-b'], stderr=subprocess.PIPE).split()})
+                                subprocess.check_output(self.kitchen_cmd + ['list', '-b', self.platform], stderr=subprocess.PIPE, env=self.env.copy()).split()})
             logger.debug('Fetching list')
 
         def __str__(self):
-            return subprocess.check_output(['kitchen', 'list']).decode()
+            return subprocess.check_output(self.kitchen_cmd + ['list', self.platform], env=self.env.copy()).decode()
 
-    def __init__(self, machines: List[str] = None, nofail:bool = False):
+    def __init__(self, machines: List[str] = None, nofail: bool = False, kitchen_cmd: List[str] = None,
+                 env: Dict[str, str] = None):
+        self.platform = ' '.join(machines)
+        self.env = env if env else environ
         self.nofail = nofail
-        self.list = self.KitchenList()
+        self.list = self.KitchenList(kitchen_cmd, self.env.copy(), platform=self.platform)
         self.process = dict()
+        self.kitchen_cmd = kitchen_cmd if kitchen_cmd else ['kitchen']
         if not machines:
             machines = ['']
         for m in machines:
             found = False
             for l in self.list:
                 if m in l:
-                    self.process[l] = subprocess.Popen(['echo', 'Initialized', l])
+                    self.process[l] = subprocess.Popen(['echo', 'Initialized', l], env=self.env.copy())
                     found = True
             if not found:
-                self.process[m] = subprocess.Popen(['echo', 'Initialized', m])
+                self.process[m] = subprocess.Popen(['echo', 'Initialized', m], env=env.copy())
         self.wait()
         self.active_machine = machines[0] if len(self.process) > 1 else set(self.process.keys()).copy().pop()
 
@@ -66,9 +73,11 @@ class Table:
         progress.close()
 
     def create(self, machine: str = ''):
+        if not machine:
+            machine = self.active_machine
         logger.debug('Creating machine {}'.format(machine if machine else 'ALL'))
         assert self.process[machine].returncode is not None, "'{}' Process is not finished".format(machine)
-        self.process[machine] = subprocess.Popen(['kitchen', 'create', machine], env=environ.copy(),)
+        self.process[machine] = subprocess.Popen(self.kitchen_cmd + ['create', machine], env=self.env.copy(), )
 
     def create_all(self):
         for m in self.process.keys():
@@ -76,30 +85,36 @@ class Table:
         self.wait()
 
     def converge(self, machine: str = ''):
+        if not machine:
+            machine = self.active_machine
         logger.debug('Converging machine {}'.format(machine if machine else 'ALL'))
         assert self.process[machine].returncode is not None, "'{}' Process is not finished".format(machine)
-        self.process[machine] = subprocess.Popen(['kitchen', 'converge', machine], env=environ.copy())
+        self.process[machine] = subprocess.Popen(self.kitchen_cmd + ['converge', machine], env=self.env.copy())
 
     def converge_all(self):
         for m in self.process.keys():
             self.converge(m)
         self.wait()
 
-    def login(self, machine:str):
+    def login(self, machine: str):
+        if not machine:
+            machine = self.active_machine
         logger.debug('Logging into machine {}'.format(machine if machine else 'ALL'))
         assert self.process[machine].returncode is not None, "'{}' Process is not finished".format(machine)
-        cmd = ['kitchen', 'login', machine]
+        cmd = self.kitchen_cmd + ['login', machine]
         logger.debug('CMD: ' + ' '.join(cmd))
-        blocked_call = subprocess.Popen(cmd, env=environ.copy())
+        blocked_call = subprocess.Popen(cmd, env=self.env.copy())
         blocked_call.wait()
 
     def verify(self, machine: str = '', test: str = ''):
+        if not machine:
+            machine = self.active_machine
         logger.debug(
             'Running tests in {} on {}'.format(test if test else 'salt://tests', machine if machine else 'ALL'))
         assert self.process[machine].returncode is not None, "'{}' Process is not finished".format(machine)
-        environ['KITCHEN_TESTS'] = test
-        logger.debug("Environment: {}".format(environ))
-        self.process[machine] = subprocess.Popen(['kitchen', 'verify', machine], env=environ.copy())
+        self.env['KITCHEN_TESTS'] = test
+        logger.debug("Environment: {}".format(self.env))
+        self.process[machine] = subprocess.Popen(self.kitchen_cmd + ['verify', machine], env=self.env.copy())
 
     def verify_all(self, test: str = ''):
         for m in self.process.keys():
@@ -107,9 +122,11 @@ class Table:
         self.wait()
 
     def destroy(self, machine: str = ''):
+        if not machine:
+            machine = self.active_machine
         logger.debug('Destroying machine {}'.format(machine if machine else 'ALL'))
         assert self.process[machine].returncode is not None, "'{}' Process is not finished".format(machine)
-        self.process[machine] = subprocess.Popen(['kitchen', 'destroy', machine], env=environ.copy())
+        self.process[machine] = subprocess.Popen(self.kitchen_cmd + ['destroy', machine], env=self.env.copy())
 
     def destroy_all(self):
         for m in self.process.keys():
@@ -148,7 +165,7 @@ if __name__ == '__main__':
     if args.expensive:
         environ['NOX_PASSTHROUGH_OPTS'] = '--run-expensive'
 
-    table = Table(args.machine, nofail=args.no_fail)
+    table = Table(args.machine, nofail=args.no_fail, env=environ)
     # If all the flags are false, then we will do all of them
     do_all = all(not flag for flag in [args.create, args.converge, args.verify, args.destroy, args.list, args.login])
     try:
@@ -170,7 +187,8 @@ if __name__ == '__main__':
         if args.list:
             exit(0)
         # Cleanup
-        if (do_all and not args.preserve) or (args.destroy and not any(flag for flag in [args.create, args.converge, args.verify, args.list])):
+        if (do_all and not args.preserve) or (
+                args.destroy and not any(flag for flag in [args.create, args.converge, args.verify, args.list])):
             table.destroy_all()
 
     logger.info("DONE!")
